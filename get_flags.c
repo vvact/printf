96d46ad8ef8e664529f4649f2bda5c7eeae201a6
Victor Midega <vbm7752@gmail.com>
2023-06-26 02:09:59 -0700

    updated getFlags.c

--- get_flags.c
+++ get_flags.c
@@ -8,27 +8,21 @@
  */
 int get_flags(const char *format, int *i)
 {
-	/* - + 0 # ' ' */
-	/* 1 2 4 8  16 */
-	int j, curr_i;
-	int flags = 0;
-	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
-	const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
-
-	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
-	{
-		for (j = 0; FLAGS_CH[j] != '\0'; j++)
-			if (format[curr_i] == FLAGS_CH[j])
-			{
-				flags |= FLAGS_ARR[j];
-				break;
-			}
-
-		if (FLAGS_CH[j] == 0)
-			break;
-	}
-
-	*i = curr_i - 1;
-
-	return (flags);
+int k, current_i;
+int flags = 0;
+const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
+const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
+for (current_i = *i + 1; format[current_i] != '\0'; current_i++)
+{
+for (k = 0; FLAGS_CH[k] != '\0'; k++)
+if (format[current_i] == FLAGS_CH[k])
+{
+flags |= FLAGS_ARR[k];
+break;
+}
+if (FLAGS_CH[k] == 0)
+break;
+}
+*i = current_i - 1;
+return (flags);
 }
